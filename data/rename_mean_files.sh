8a9ffb4cd7b9c0b9aec362b85179bdccb4fbe04f
Jimmy Wyngaarden <james.wyngaarden@temple.edu>
2021-01-30 14:05:50 -0500

    update rename_mean_files.sh

--- data/rename_mean_files.sh
+++ data/rename_mean_files.sh
@@ -1,15 +1,14 @@
+
 #!/usr/bin
 
-# this script will copy mean images produced by realign step to match naming format for preprocessing script
+# this script will add T1w files to subject directories
 
-# user inputs:
-
+# list subjects whose data you're working with:
 Subs="3836 3845 3846 3847 3848 3849 3851 3852 3854 3855 3864 3865 3871 3877 3880 3882 3883 3886 3887 3889 3890 3891 3892 3893 3895 3896 3910 3912 3914 3920 3967 3992 4017 4018 4019 4020 4069"
-input_dir=/data/projects/STUDIES/nm_jw/data/s
+maindir="/data/projects/STUDIES/nm_jw/data/s"
 
 for sub in $Subs
 do
-        cp /data/projects/STUDIES/nm_jw/s${sub}/s${sub}_nonnormed_NM.nii /data/projects/STUDIES/nm_jw/s${sub}/s${sub}_SDC_NM.nii
-
+        cp ${maindir}${sub}/s${sub}_SDC_T1w.nii ${maindir}${sub}/s${sub}_SDC_T1w.nii
+        
 done
-
